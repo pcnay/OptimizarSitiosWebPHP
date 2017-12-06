a6d33e01e09146a9b6daee00f166b2eaac7df5f1
Ramon <pcnay2003@gmail.com>
2017-12-05 21:01:15 -0800

    Creando el Index.php sin estilos CSS

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html > 
+<html lang="es">
+  <head>
+    <meta charset = "UTF-8" />
+    <title>Aplicacion CRUD De Super Heroes</title>
+    <meta name= "description" content="Aplicacion CRUD con Filosofia MVC desarrollada en PHP y Ajax"/>
+    <link rel="stylesheet" href="css/super-heroes.css" />
+
+
+  </head>
+  
+  <body>
+    <header id="cabecera">
+      <h1>Super Heroes</h1>
+      <div><img src="img/super-heroes.png" alt = "Super Heroes"/></div>
+      <!-- Se coloca "#" ya que no se requiere que vaya algun enlace. -->
+      <a href="#" id="insertar">Insertar</a>
+
+    </header>
+    </section id = "contenido">
+      <p>Aca va el contenido.</p>
+    </section>
+    <script src="js/super-heroes.js"></script>
+  </body>
+</html>
