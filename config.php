9563b2e530fab07e8fd32cd21da4bc083df1c568
Ramon <pcnay2003@gmail.com>
2017-12-26 22:06:08 -0800

    Se borrar archivos que no corresponden a la carpeta

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-  # Definiendo las constantes, son por convension a Mayusculas.
-  define ("SERVER","localhost");
-  define ("USER","root");
-  define ("PASS","");
-  define ("BD","super_heroes");
-?>
